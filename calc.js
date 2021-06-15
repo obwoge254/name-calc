c2a9fede4a3cfe91114488cd20f6468718801b37
obwoge254 <nelson.obwoge@student.moringaschool.com>
2021-06-15 22:49:36 +0300

    modified the jsfile

--- calc.js
+++ calc.js
@@ -7,15 +7,27 @@ function calcDayOfBirth(){
 
  //calculate of  day of week
 
- myDayOfweek  = dt.getUTCDay();
+ myDayOfweek  = dt.getDay();
 
- // validation 
+ // validation
+ 
+ var dt;
+bday = window.prompt("When is your birthday (YYYY-MM-DD)?","");
+var bdayArray = bday.split('-');
+
+if(bdayArray.length !== 3){
+    alert("invalid Date");
+}else{
+   if(!bdayArray[0].match(/^\d\d\d\d$/) || 
+      !bdayArray[1].match(/^\d\d$/) || 
+      !bdayArray[2].match(/^\d\d$/)){
+       alert("invalid Date");    
  
  //choose name either male/female
 
- male = [Sunday: Kwasi Monday: Kwadwo Tuesday: Kwabena Wednesday: Kwaku Thursday:  Yaw Friday: Kofi Saturday: Kwame];
+ //male = [Sunday: Kwasi Monday: Kwadwo Tuesday: Kwabena Wednesday: Kwaku Thursday:  Yaw Friday: Kofi Saturday: Kwame];
 
- female = [Sunday: Akosua, Monday: Adwoa Tuesday: Abenaa Wednesday: Akua Thursday:  Yaa Friday: Afua Saturday: Ama];
+ //female = [Sunday: Akosua, Monday: Adwoa Tuesday: Abenaa Wednesday: Akua Thursday:  Yaa Friday: Afua Saturday: Ama];
  //displaythe result
  document.getElementById('play').innerHTML = "Your day of week " + myDayOfweek; 
 }
