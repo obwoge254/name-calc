bd835ebe5d16358916a824f3769dff14ef9e3046
obwoge254 <nelson.obwoge@student.moringaschool.com>
2021-06-15 14:50:20 +0300

    modified jsfile

--- calc.js
+++ calc.js
@@ -2,9 +2,17 @@
 function calcDayOfBirth(){ 
     event.preventDefault();
  //get date of birth
- var date= new Date(document.getElementById("mydob").value);
+ var  dt = new Date(document.getElementById("mydob").value);
+
+
  //calculate of  day of week
+
+ myDayOfweek  = dt.getDay() ;
+// validation 
+
+
+
  //choose name either male/female
  //displaythe result
-
+ document.getElementById('play').innerHTML = "Your year of birth is " + myYearOfBirth; 
 }
