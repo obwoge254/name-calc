26eeca3ec671cb007fc84bc25d4758e6e7d95e5c
obwoge254 <nelson.obwoge@student.moringaschool.com>
2021-06-15 12:11:54 +0300

    modified jsfile

--- calc.js
+++ calc.js
@@ -1,26 +1,6 @@
 
 function calcDayOfBirth(){ 
     event.preventDefault();
-//validate
-if(bdayArray.length !== 3){
-    alert("invalid Date");
-}else{
-   if(!bdayArray[0].match(/^\d\d\d\d$/) || 
-      !bdayArray[1].match(/^\d\d$/) || 
-      !bdayArray[2].match(/^\d\d$/)){
-       alert("invalid Date");    
-   }else{
-       var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday', 'Friday', 'Saturday'];     
-       var currentTime = new Date(            
-           parseInt(bdayArray[0]),
-           parseInt(bdayArray[1]) - 1, //month starts from 0
-           parseInt(bdayArray[2])
-       );        
-       var currentDay = currentTime.getDay();       
-       var currentDayName= days[currentDay];
-      document.write("You were born on "+currentDayName);
-   }
-}
 
 
 }
