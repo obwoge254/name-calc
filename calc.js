10155f778ddb4403835065dd4342155c62614b69
obwoge254 <nelson.obwoge@student.moringaschool.com>
2021-06-16 11:02:58 +0300

    modified file

--- calc.js
+++ calc.js
@@ -7,32 +7,32 @@ function calcDayOfBirth(){
 
  //calculate of  day of week
 
- myDayOfweek  = dt.getDay();
+ //myDayOfweek  = dt.getDay();
 
  // validation
  
  var dt;
-bday = window.prompt("When is your birthday (YYYY-MM-DD)?","");
-var bdayArray = bday.split('-');
+//bday = window.prompt("When is your birthday (YYYY-MM-DD)?","");
+var dtArray = dt.split('-');
 
-if(bdayArray.length !== 3){
+if(dtArray.length !== 3){
     alert("invalid Date");
 }else{
-   if(!bdayArray[0].match(/^\d\d\d\d$/) || 
-      !bdayArray[1].match(/^\d\d$/) || 
-      !bdayArray[2].match(/^\d\d$/)){
+   if(!dtArray[0].match(/^\d\d\d\d$/) || 
+      !dtArray[1].match(/^\d\d$/) || 
+      !dtArray[2].match(/^\d\d$/)){
        alert("invalid Date");  
        
     }else{
         var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday', 'Friday', 'Saturday'];     
         var currentTime = new Date(            
-            parseInt(bdayArray[0]),
-            parseInt(bdayArray[1]) - 1, 
-            parseInt(bdayArray[2])
+            parseInt(dtArray[0]),
+            parseInt(dtArray[1]) - 1, 
+            parseInt(dtArray[2])
         ); 
         var currentDay = currentTime.getDay();       
         var currentDayName= days[currentDay];
-       document.write("You were born on "+currentDayName);
+       document.getElementById('display').innerHTML = "You were born on "+currentDayName;
     }
 }
  
@@ -42,6 +42,6 @@ if(bdayArray.length !== 3){
 
  //female = [Sunday: Akosua, Monday: Adwoa Tuesday: Abenaa Wednesday: Akua Thursday:  Yaa Friday: Afua Saturday: Ama];
  //displaythe result
- document.getElementById('display').innerHTML = "Your day of week " + myDayOfweek; 
+ //document.getElementById('display').innerHTML = "Your day of week " + myDayOfweek; 
  
 }
